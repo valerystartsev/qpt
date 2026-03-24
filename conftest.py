1fb8d7fcc4cfb6caa85754a24e4f533f4f36973a
valerystartsev <valery.startsev@gmail.com>
2026-03-24 11:07:37 +0300

    headless true

--- conftest.py
+++ conftest.py
@@ -15,7 +15,7 @@ from allure_commons.types import AttachmentType
 @pytest.fixture()
 def driver():
     options = Options()
-    #options.add_argument('--headless=new')
+    options.add_argument('--headless=new')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
