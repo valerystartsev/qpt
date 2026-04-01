6db502b256e6d2223593a398bef8d060015fe275
valerystartsev <valery.startsev@gmail.com>
2026-04-01 14:40:24 +0300

    add locked user login test

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -10,6 +10,14 @@ def test_incorrect_pass(login_page):
     )
 
 
+def test_locked_pass(login_page):
+    login_page.open()
+    login_page.fill_login_form(login='locked_out_user', passw='secret_sauce')
+    assert login_page.error_text == (
+        'Epic sadface: Sorry, this user has been locked out.'
+    )
+
+
 """
 Epic sadface: Username and password do not match any user in this service
 """
