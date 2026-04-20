65af4858a82e69bf28d7aa4e454cfa3dd9e2ada8
valerystartsev <valery.startsev@gmail.com>
2026-04-20 16:08:56 +0300

    add locked user login test

--- conftest.py
+++ conftest.py
@@ -25,7 +25,7 @@ def driver():
     }
     options.add_experimental_option('prefs', chrome_prefs)
 
-    #options.add_argument('--headless=new')
+    options.add_argument('--headless=new')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
